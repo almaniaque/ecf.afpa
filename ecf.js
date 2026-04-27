69ca4ea5f0edbdc5c4b5252965c80f45610b21c5
davy Thiebaut <davy.du.60@hotmail.fr>
2026-04-27 14:16:44 +0200

    bibliotheque

--- ecf.js
+++ ecf.js
@@ -14,3 +14,25 @@ menuElements.forEach(menu => {
 
     });
 });
+
+const navElement = document.getElementById("navi");
+
+navElement.addEventListener("click", () => {
+    let menuElements = document.querySelectorAll(".menu");
+
+});
+
+for (let i = 0; i < menuElements.length; i++) {
+    menuElements[i].addEventListener("click", (event) => {
+        alert(event.target.innerHTML);
+        event.preventDefault()
+    });
+}
+
+const searchElement = document.getElementById("search")
+
+searchElement.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        alert(searchElement.value)
+    }
+})
